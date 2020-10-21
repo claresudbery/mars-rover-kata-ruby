8b52c460b7f383d3b0fbc4a3b74feebee45b4f60
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-21 21:49:05 +0100

    Move more functions into AppHelper class.

--- webapp.rb
+++ webapp.rb
@@ -23,7 +23,7 @@ class WebApp < Sinatra::Base
         handle_exceptions do
             update_grid
             instructions = params["instructions"]
-            process_instructions(instructions, session[:mars_rovers], session[:grid], MarsRoverFactory.new) do
+            AppHelper::process_instructions(instructions, session[:mars_rovers], session[:grid], MarsRoverFactory.new) do
                 update_display
             end
         end
@@ -59,32 +59,6 @@ class WebApp < Sinatra::Base
         end
     end
 
-    def start_rover(instructions, mars_rovers, grid, mars_rover_factory)
-        new_rover = AppHelper::convert_first_input(instructions) 
-        rover = mars_rover_factory.generate_rover(new_rover[:name], new_rover[:type])
-        rover.start(new_rover[:x], new_rover[:y], new_rover[:direction], grid)
-        grid.update(rover)
-        mars_rovers[new_rover[:name]] = rover
-    end
-
-    def move_rover(instructions, rovers, grid)  
-        instructions = instructions.split(",")
-        rover_name = instructions[0]
-        instructions.shift # removes first element
-        instructions.each do |movement|   
-            process_movement(movement, rovers[rover_name], grid)
-        end
-    end
-
-    def process_movement(movement, rover, grid)    
-        if is_turn?(movement)       
-            rover.turn(movement)
-        else
-            rover.move(movement, grid)
-        end
-        grid.update(rover)
-    end
-
     def show_error(error)
         @output = update_display + error + "\n\n"
     end
@@ -94,22 +68,5 @@ class WebApp < Sinatra::Base
         @output = @output + AppHelper::USER_INFORMATION + "\n\n"
         @output = @output + AppHelper::REQUEST_FOR_FURTHER_INPUT + "\n\n"
         @output
-    end
-
-    def process_instructions(instructions, rovers, grid, rover_factory)
-        if is_movement?(instructions)
-            move_rover(instructions, rovers, grid)
-        else
-            start_rover(instructions, rovers, grid, rover_factory)
-        end
-        yield
-    end
-
-    def is_turn?(movement)
-        AppHelper::TURNS.include?(movement)
-    end
-
-    def is_movement?(movement)
-        AppHelper::MOVEMENTS.include?(movement[movement.length-1])
     end
 end
