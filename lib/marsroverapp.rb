d3d4eb18dafed22a52b1bb510141afeef02d79f4
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-21 17:52:35 +0100

    Instructions that reflect the fact flying rover and straight line rover not implemented yet

--- lib/marsroverapp.rb
+++ lib/marsroverapp.rb
@@ -3,8 +3,9 @@ require_relative 'exceptions/obstacle_exception'
 require_relative 'exceptions/sky_high_obstacle_exception'
 
 class MarsRoverApp
-    USER_INFORMATION = "There are three types of Rover: Straight-line rover = 'SLR', Rover360 = '360', FlyingRover = 'FLY'"
-    REQUEST_FOR_FIRST_INPUT = "Please input a 3-letter name, type, start coordinates and a direction for your Rover - eg ANN,SLR,0,0,N"
+    USER_INFORMATION_ALL_ROVERS = "There are three types of Rover: Straight-line rover = 'SLR', Rover360 = '360', FlyingRover = 'FLY'"
+    USER_INFORMATION = "You may have heard rumours of flying rovers and straight-line rovers, but these are still under construction. For now we only have Rover360s. When creating a new rover, use '360' for type."
+    REQUEST_FOR_FIRST_INPUT = "Please input a 3-letter name, type, start coordinates and a direction for your Rover - eg ANN,360,0,0,N"
     REQUEST_FOR_FURTHER_INPUT = "Please input, comma-separated, either rover name followed by a sequence of the following single chars: f(forwards), b(backwards), l(left), r(right) - eg 'ANN,r,f,f' ... or a 3-letter name, start coordinates, type and a direction for a new Rover - eg 'MIN,360,0,0,N'"
     BAD_INPUT_ERROR = "Sorry, I don't understand that input."
     OBSTACLE_ERROR = "Oh no, I'm sorry, I can't process that instruction. There is an obstacle in the way!"
