5d5a6abca7e59b1bed390a83cfead9c734db8ebb
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-02-11 12:08:49 +0000

    Better instructions, better formatted.

--- lib/app_helper.rb
+++ lib/app_helper.rb
@@ -4,9 +4,9 @@ require_relative 'exceptions/sky_high_obstacle_error'
 
 class AppHelper
     USER_INFORMATION_ALL_ROVERS = "There are three types of Rover: Straight-line rover = 'SLR', Rover360 = '360', FlyingRover = 'FLY'"
-    USER_INFORMATION = "You may have heard rumours of flying rovers and straight-line rovers, but these are still under construction. For now we only have Rover360s. When creating a new rover, use '360' for type."
+    USER_INFORMATION = "You may have heard rumours of flying rovers and straight-line rovers, but these are still under construction. \nFor now we only have Rover360s. When creating a new rover, use '360' for type."
     REQUEST_FOR_FIRST_INPUT = "Please input a 3-letter name, type, start coordinates and a direction for your Rover - eg ANN,360,0,0,N"
-    REQUEST_FOR_FURTHER_INPUT = "Please input, comma-separated, either rover name followed by a sequence of the following single chars: f(forwards), b(backwards), l(left), r(right) - eg 'ANN,r,f,f' ... or a 3-letter name, start coordinates, type and a direction for a new Rover - eg 'MIN,360,0,0,N'"
+    REQUEST_FOR_FURTHER_INPUT = "Please input, comma-separated, one of the following: \n\nTo create a new rover: 3-letter name, start coordinates, type and a direction for a new Rover - eg 'MIN,360,0,0,N' \nTo direct a rover: Rover name followed by a sequence of the following single chars: f(forwards), b(backwards), l(left), r(right) - eg 'ANN,r,f,f' \nNB: You can't direct a rover until you create it."
     MOVEMENTS = [StraightLineRover::LEFT, StraightLineRover::RIGHT, StraightLineRover::FORWARD, StraightLineRover::BACKWARD]
     TURNS = [StraightLineRover::LEFT, StraightLineRover::RIGHT]
 
